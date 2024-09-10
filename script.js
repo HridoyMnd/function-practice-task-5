c5be7d95bb9d3326ea8bc314d9eeb17c92442686
Hridoy Mnd <hridoymnd10@gmail.com>
2024-09-10 14:56:58 +0600

    practice task 5 done

---
+++ script.js
@@ -0,0 +1,17 @@
+///function practice task 5
+
+function odd_even(number) {
+
+if(number % 2 === 1){
+    return "This is odd  number";
+}
+else {
+    return "This is even number";
+}
+
+
+}
+
+
+let result = odd_even(4);
+console.log(result);
